7cd2f104d8cd7fa1d029c1c067c026f8c072cf82
Fakhiha Rameen <115946749+fakhiha@users.noreply.github.com>
2023-06-03 14:19:34 +0500

    added firebase script to authentication manager

--- Assets/Scenes/MainMenu/Scripts/AuthManager.cs
+++ Assets/Scenes/MainMenu/Scripts/AuthManager.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
 
 public class AuthManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser User;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Login")]
+    public TMP_InputField emailLoginField;
+    public TMP_InputField passwordLoginField;
+    
+
+    [Header("Register")]
+    public TMP_InputField usernameRegisterField;
+    public TMP_InputField emailRegisterField;
+    public TMP_InputField passwordRegisterField;
+   
+
 }
